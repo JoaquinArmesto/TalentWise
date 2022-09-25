04ae2f2b1fb395d75505c8ace2b6bb938b787166
Joaquin Armesto <89107170+JoaquinArmesto@users.noreply.github.com>
2022-09-25 14:38:18 -0300

    Delete recruiting_model_drops.sql

--- recruiting_model_drops.sql
+++
@@ -1,8 +0,0 @@
-drop trigger if exists trRequisitionUpdateLog;
-drop trigger if exists trRequisitionUpdate;
-DROP FUNCTION IF EXISTS BusinessDays;
-DROP FUNCTION IF EXISTS ElapsedBusinessDaysForRole;
-DROP PROCEDURE IF EXISTS SptableSort;
-DROP PROCEDURE IF EXISTS SpCloseOpenProject;
-DROP PROCEDURE IF EXISTS SpOpenCloseAllProjects;
-
