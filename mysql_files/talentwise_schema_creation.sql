46778fb8061c64d9028686363f07869441d5bfbf
JoaquinArm <alvaro.armesto@gmail.com>
2022-10-01 18:27:02 -0300

    Update

--- mysql_files/talentwise_schema_creation.sql
+++ mysql_files/talentwise_schema_creation.sql
@@ -1,17 +1,17 @@
 -- Dropping the schema for reruns
-DROP SCHEMA IF EXISTS talent_wise;
+drop schema if exists talent_wise;
 
 
 -- Creating the schema
-CREATE SCHEMA IF NOT EXISTS talent_wise;
+create schema if not exists talent_wise;
 
 
 -- Use the correct schema
-USE talent_wise;
+use talent_wise;
 
 
 -- Creating roster table
-CREATE TABLE IF NOT EXISTS roster (
+create table if not exists roster (
 	id bigint auto_increment,
     created_at timestamp default (current_timestamp),
     updated_at timestamp default (current_timestamp),
@@ -21,24 +21,22 @@ CREATE TABLE IF NOT EXISTS roster (
     role text(50) not null,
     email varchar(255) not null,
     phone varchar (255),
-	CONSTRAINT PK_roster PRIMARY KEY (id)
-);
+	constraint PK_roster primary key (id));
 
 
 -- Create table app_user for app usage
-CREATE TABLE IF NOT EXISTS app_user (
+create table if not exists app_user (
     id bigint auto_increment,
     user_name text not null,
     user_pass text not null,
     user_email varchar(255) not null,
     roster_id bigint not null,
-    CONSTRAINT PK_app_user_id PRIMARY KEY (id),
-    CONSTRAINT FK_app_roster_id FOREIGN KEY (roster_id) REFERENCES roster (id)
-);
+    constraint PK_app_user_id primary key (id),
+    constraint FK_app_roster_id foreign key (roster_id) references roster (id));
 
 
 -- Creating client table
-CREATE TABLE IF NOT EXISTS client (
+create table if not exists client (
     id bigint auto_increment,
     created_at timestamp default(current_timestamp),
     updated_at timestamp default(current_timestamp),
@@ -52,26 +50,24 @@ CREATE TABLE IF NOT EXISTS client (
     revenue_year bigint default(null),
     operation_costs bigint default(null),
     earnings bigint default(null),
-    CONSTRAINT PK_client_id PRIMARY KEY (id),
-    CONSTRAINT FK_internal_manager_id FOREIGN KEY (internal_manager_id) REFERENCES roster (id)
-);
+    constraint PK_client_id primary key (id),
+    constraint FK_internal_manager_id foreign key (internal_manager_id) references roster (id));
 
 
 -- Creating project table
-CREATE TABLE IF NOT EXISTS project (
+create table if not exists project (
 	id bigint auto_increment,
 	client_id bigint not null,
 	project_name varchar(255) not null,
 	created_at timestamp default (current_timestamp),
     updated_at timestamp default (current_timestamp),
     deleted_at timestamp null,
-	CONSTRAINT PK_project_id PRIMARY KEY (id),
-	CONSTRAINT FK_client_id FOREIGN KEY (client_id) REFERENCES client (id)
-);
+	constraint PK_project_id primary key (id),
+	constraint FK_client_id foreign key (client_id) references client (id));
 
 
 -- Creating role table
-CREATE TABLE IF NOT EXISTS role (
+create table if not exists role (
 	id bigint auto_increment,
     created_at timestamp default (current_timestamp),
     updated_at timestamp default (current_timestamp),
@@ -81,36 +77,33 @@ CREATE TABLE IF NOT EXISTS role (
     end_date datetime null,
     client varchar(255) not null,
     project_id bigint not null,
-    CONSTRAINT PK_roles_id PRIMARY KEY (id),
-    CONSTRAINT FK_project_id FOREIGN KEY  (project_id) REFERENCES project (id)
- );
+    constraint PK_roles_id primary key (id),
+    constraint FK_project_id foreign key  (project_id) references project (id));
 
 
 -- Creating country table
-CREATE TABLE IF NOT EXISTS country (
+create table if not exists country (
     id bigint auto_increment,
     ISO text(2) not null,
     ISO3 text(3) not null,
     numeric_code int not null,
     name varchar(255) not null,
-    CONSTRAINT PK_country_id PRIMARY KEY (id)
-);
+    constraint PK_country_id primary key (id));
 
 
 -- Creating city table
-CREATE TABLE IF NOT EXISTS city (
+create table if not exists city (
     id bigint auto_increment,
     name text,
     lat bigint not null,
     lng bigint not null,
     country_id bigint not null,
-    CONSTRAINT PK_city_id PRIMARY KEY (id),
-    CONSTRAINT FK_country_id FOREIGN KEY (country_id) REFERENCES country (id)
-);
+    constraint PK_city_id primary key (id),
+    constraint FK_country_id foreign key (country_id) references country (id));
 
 
 -- Creating requisition table
-CREATE TABLE IF NOT EXISTS requisition (
+create table if not exists requisition (
     id bigint auto_increment,
     created_at timestamp default (current_timestamp),
     updated_at timestamp default (current_timestamp),
@@ -120,25 +113,23 @@ CREATE TABLE IF NOT EXISTS requisition (
     seniority text NOT NULL,
     role_id bigint NOT NULL,
     city_id bigint NOT NULL,
-    CONSTRAINT PK_requisition_id PRIMARY KEY (id),
-    CONSTRAINT FK_role_id FOREIGN KEY (role_id) REFERENCES role (id),
-    CONSTRAINT FK_city_id FOREIGN KEY (city_id) REFERENCES city (id)
-);
+    constraint PK_requisition_id primary key (id),
+    constraint FK_role_id foreign key (role_id) references role (id),
+    constraint FK_city_id foreign key (city_id) references city (id));
 
 
 -- Creating candidate table
-CREATE TABLE IF NOT EXISTS candidate (
+create table if not exists candidate (
     id bigint auto_increment,
     name text not null,
     email varchar (255) not null,
     phone varchar (255) not null,
     linkedin varchar (255) not null,
-    CONSTRAINT PRIMARY KEY (id)
-);
+    constraint primary key (id));
 
 
 -- Creating stage table
-CREATE TABLE IF NOT EXISTS stage (
+create table if not exists stage (
     id int auto_increment,
     created_at timestamp default(current_timestamp),
     updated_at timestamp default(current_timestamp),
@@ -146,37 +137,34 @@ CREATE TABLE IF NOT EXISTS stage (
     name text not null,
     description text not null,
     notes text null,
-    CONSTRAINT PRIMARY KEY (id)
-);
+    constraint primary key (id));
 
 
 -- Creating funnel table
-CREATE TABLE IF NOT EXISTS funnel (
+create table if not exists funnel (
     id bigint auto_increment,
     date datetime default(current_date),
     candidate_id bigint not null,
     stage_id int not null,
     requisition_id bigint not null,
     roster_id bigint not null,
-    CONSTRAINT PK_funnel_activity PRIMARY KEY  (id),
-    CONSTRAINT FK_candidate_id FOREIGN KEY (candidate_id) REFERENCES candidate (id),
-    CONSTRAINT FK_stage_id FOREIGN KEY (stage_id) REFERENCES stage (id),
-    CONSTRAINT FK_requisition_id FOREIGN KEY (requisition_id) REFERENCES requisition (id),
-    CONSTRAINT FK_roster_id FOREIGN KEY (roster_id) REFERENCES roster (id)
-);
+    constraint PK_funnel_activity primary key  (id),
+    constraint FK_candidate_id foreign key (candidate_id) references candidate (id),
+    constraint FK_stage_id foreign key (stage_id) references stage (id),
+    constraint FK_requisition_id foreign key (requisition_id) references requisition (id),
+    constraint FK_roster_id foreign key (roster_id) references roster (id));
 
 
-CREATE TABLE IF NOT EXISTS log_funnel_new_entry (
+create table if not exists log_funnel_new_entry (
     id bigint auto_increment,
     user varchar(255) not null,
     date date default(current_date),
     time time default(current_time),
     funnel_id int not null,
-    CONSTRAINT PK_logFunnelNewEntries PRIMARY KEY (id)
-);
+    constraint PK_logFunnelNewEntries primary key (id));
 
 
-CREATE TABLE IF NOT EXISTS log_funnel_new_state (
+create table if not exists log_funnel_new_state (
     id bigint auto_increment,
     funnel_id bigint,
     created_at timestamp default(current_timestamp),
@@ -184,21 +172,19 @@ CREATE TABLE IF NOT EXISTS log_funnel_new_state (
     value_before varchar(255),
     value_after varchar(255),
     modified int not null default(0),
-    CONSTRAINT PK_logFunnelUpdatesId PRIMARY KEY (id),
-    CONSTRAINT FK_logFunnelUpdatesId FOREIGN KEY (funnel_id) REFERENCES funnel (id)
-);
+    constraint PK_logFunnelUpdatesId primary key (id),
+    constraint FK_logFunnelUpdatesId foreign key (funnel_id) references funnel (id));
 
 
-CREATE TABLE IF NOT EXISTS log_requisition_update (
+create table if not exists log_requisition_update (
     id bigint auto_increment,
     requisition_id bigint not null,
     user varchar(255) not null,
     date date default(current_date),
     time time default(current_time),
-    CONSTRAINT PK_logFunnelUpdateId PRIMARY KEY (id),
-    CONSTRAINT FK_logRequisitionUpdateRequisitionId FOREIGN KEY (requisition_id) REFERENCES requisition (id)
-);
+    constraint PK_logFunnelUpdateId primary key (id),
+    constraint FK_logRequisitionUpdateRequisitionId foreign key (requisition_id) references requisition (id));
 
 
-CREATE UNIQUE INDEX idx_funnel_alt_pk
+create unique index idx_funnel_alt_pk
 on funnel (candidate_id, stage_id, requisition_id);
